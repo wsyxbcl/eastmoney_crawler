cb7d1ba9085cadc804e6bf5ce1572a88308a28f4
wsyxbcl <wsyxbcl@gmail.com>
2019-06-19 17:13:25 +0800

    rewrite path relevent stuff

--- eastmoney_nbyj.py
+++ eastmoney_nbyj.py
@@ -1,18 +1,20 @@
 import os
 import requests
 import re
-import pandas as pd
 import time
 import random
 import sys
+from pathlib import Path
 
-def Save(save_path, filename, item):
+import pandas as pd
+
+def save_report(save_path, filename, item):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    path = save_path+"/"+filename+".csv"
+    path = Path(save_path).joinpath(filename)
     with open(path, "w+") as fp:
         #fp.write('datetime,insName,insStar,rate,secuFullCode,secuName,sys16, sys17, sys18,,,title')
-        fp.write('test1,test2, test3, test4, test5, test6, test7 ,test8,test9,test10,test11,test12,test13,test14,test15,test16,test17,test18,test19')
+        fp.write('test1, test2, test3, test4, test5, test6, test7 ,test8,test9,test10,test11,test12,test13,test14,test15,test16,test17,test18,test19')
         fp.write('\n')
         for i in item:
             for j in i:
@@ -43,22 +45,22 @@ def Page_Info(myPage):
 
 print("start")
 print("This is a crawler that extracts data from http://data.eastmoney.com/bbsj/201512/yjbb.html")
-save_path = u"eastmoney_nbyj"
+save_path = "eastmoney_nbyj"
 year = input("Give me the date(e.g. 2016-09-30): ")
-filename = "nbyj_" + year
+filename = "nbyj_" + year + '.csv'
 text = ""
 num = int(input("How many pages do you except the crawler to walk through? "))
 for i in range(1, num + 1):
-    url = u"http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=SR&sty=YJBB&fd=" + year + "&st=13&sr=-1&p=" + str(i) + "&ps=50&js=var%20wMihohub={pages:(pc)"
+    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=SR&sty=YJBB&fd=" + year + "&st=13&sr=-1&p=" + str(i) + "&ps=50&js=var%20wMihohub={pages:(pc)"
     text = text + Crawler(url) + '\n'
     time.sleep(0 + random.randint(0, 2))
 print("Finish downloading")
-p = re.compile(u'"(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?)"')
+p = re.compile('"(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?)"')
 print("finding pattern")
 item = p.findall(text)
 print("saving")
-Save(save_path, filename, item)
-df = pd.read_csv(save_path+"/"+filename+".csv", dtype = str, index_col = False, sep = ',')
+save_report(save_path, filename, item)
+df = pd.read_csv(Path(save_path).joinpath(filename), dtype = str, index_col = False, sep = ',')
 
 for i in range(len(df.index)):
     # df.set_value(i, 'test1', '='+'"'+df.values[i][0]+'"') # for excel
