811fc7f770adb1744e491e5867dfe16392686487
wsyxbcl <wsyxbcl@gmail.com>
2019-06-19 16:51:12 +0800

    change encoding

--- eastmoney_nbyj.py
+++ eastmoney_nbyj.py
@@ -58,9 +58,9 @@ print("finding pattern")
 item = p.findall(text)
 print("saving")
 Save(save_path, filename, item)
-df = pd.read_csv(save_path+"/"+filename+".csv", dtype = str, encoding = 'gbk', index_col = False, sep = ',')
+df = pd.read_csv(save_path+"/"+filename+".csv", dtype = str, index_col = False, sep = ',')
 for i in range(len(df.index)):
     df.set_value(i, 'test1', '=' + '"' + df.values[i][0] + '"')
     
-df.to_csv("eastmoney_nbyj/nbyj_" + year + ".csv", encoding = 'gbk', index = False)
+df.to_csv("eastmoney_nbyj/nbyj_" + year + ".csv", index = False)
 print("end")
